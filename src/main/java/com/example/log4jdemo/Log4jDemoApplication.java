05d6ba108ba52b7f93022441068801c206a24ddd
Sanidhya <54096594+sanidhyamesh@users.noreply.github.com>
2022-01-14 16:26:46 +0530

    Update Log4jDemoApplication.java

--- src/main/java/com/example/log4jdemo/Log4jDemoApplication.java
+++ src/main/java/com/example/log4jdemo/Log4jDemoApplication.java
@@ -1,1 +1,18 @@
+package com.example.log4jdemo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Log4jDemoApplication {
+	public static final Logger LOGGER = LogManager.getLogger(Log4jDemoApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(Log4jDemoApplication.class, args);
+		LOGGER.info("Hello world");
+	}
+
+}
 
